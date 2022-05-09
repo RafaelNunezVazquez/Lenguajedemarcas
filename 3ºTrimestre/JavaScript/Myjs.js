60c14dfe4ae40d1009a8a8a64873e12f64691e34
Rafael Nuñez <91255999+RafaelNunezVazquez@users.noreply.github.com>
2022-05-09 20:14:22 +0200

    cambios

---
+++ 3ºTrimestre/JavaScript/Myjs.js
@@ -0,0 +1,13 @@
+function cambiaParrafo() {
+    document.getElementById("demo").innerHTML = "parrafo cambiado.";
+  }
+  function borraParrafo() {
+    document.getElementById("demo").style.display = 'none';
+  }
+  function apareceparrafo() {
+    document.getElementById("demo").style.display = 'block';
+  }
+  
+  function escritora() {
+  document.getElementById("demo").innerHTML = 'Hello world! im very bored';
+  }
